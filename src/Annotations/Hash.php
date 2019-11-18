43cf955d8d768232d4633574b77ffd15c7e25f12
Jurriaan <jur@oberon.nl>
2019-11-18 17:29:50 +0100

    hash strategy

--- src/Annotations/Hash.php
+++ src/Annotations/Hash.php
@@ -7,9 +7,23 @@
 
 namespace Oberon\Anonymize\Annotations;
 
+/**
+ * @Annotation
+ * @Target({"PROPERTY"})
+ * @Attributes({
+ *   @Attribute("algorithm", type = "string"),
+ *   @Attribute("salt", type = "string"),
+ * })
+ */
 class Hash extends AnonymizeProperty
 {
-    public $algorithm;
+    /**
+     * Any of the algorithms listed by hash_algos()
+     * @var string
+     */
+    public $algorithm = 'md5';
+    /** @var string */
+    public $salt = '';
 
     public function getStrategy(): string
     {
